0091741ba341124985498040ac2eecb1c7970ceb
Rogeshwar <rogeshwar@gmail.com>
2025-02-02 18:19:21 +0530

    Fix : Custom exception

--- Demo/src/custom.java
+++ Demo/src/custom.java
@@ -1,4 +1,27 @@
-
+class InvalidAgeException extends Exception{
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
 public class custom {
-
+	
+	public static void withdraw(int amount) throws InvalidAgeException{
+		if(amount<0)
+		{
+			throw new InvalidAgeException ("amount is negative");
+		}
+		else
+		{
+			System.out.println("the amount withdraw : "+amount);
+		}
+	}
+	public static void main(String[]args) {
+		try
+		{
+			withdraw(-500);
+		}
+		catch(InvalidAgeException e) {
+			System.out.println("Exception caught : "+ e.getMessage());
+		}
+	}
 }
